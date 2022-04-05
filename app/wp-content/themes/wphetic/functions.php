2aad0593a67cf4dca01531f7692731ff50f0e849
Adrien <adrien.bouteiller01@gmail.com>
2022-04-05 17:06:56 +0200

    login sans redirection

--- app/wp-content/themes/wphetic/functions.php
+++ app/wp-content/themes/wphetic/functions.php
@@ -1,7 +1,8 @@
 <?php
 add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+
 function prefix_disable_gutenberg($current_status, $post_type){}
 
-wp_login_form( ['form_id' => 'wphetic_login_form'] )
+
 
 ?>
--- app/wp-content/themes/wphetic/header.php
+++ app/wp-content/themes/wphetic/header.php
@@ -2,6 +2,8 @@
 <head>
     <title>Indestructible uncanny</title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+    <?php wp_head() ?>
 </head>
 <body>
 <header id="wp_header" class="header">
