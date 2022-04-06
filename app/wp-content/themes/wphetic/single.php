5d2a62eff4e5d89ed014e4c12f0c834c5dd8c908
Tristan Seclet <tristanseclet@gmail.com>
2022-04-06 14:14:24 +0200

    comments

--- app/wp-content/themes/wphetic/single.php
+++ app/wp-content/themes/wphetic/single.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* Template Name: Recipe
+* Template Name: Login
 * Template Post Type : page
 */
 ?>
@@ -20,20 +20,7 @@
         <p class="post_description"><?php the_content(__('(more...)')); ?></p>
         <div class="post_comments">
             <span class="post_count_comments"><?= get_comments_number() ?></span>
-            <?php if (have_comments()) : while (have_comments()) : the_comment(); ?>
-                <div class=""> pd pd</div>
-                <div class="post_comment">
-                    <div class="comment_meta">
-                        <span class="comment_author"> <?= the_author() ?></span>
-                        <span class="comment_date"><?php the_time('F jS, Y') ?></span>
-                    </div>
-                    <p class="comment_content"><?= the_comment()?></p>
-                </div>
-            <?php endwhile; else : ?>
-                <div class="post_comment">
-                    Aucun commentaire.
-                </div>
-            <?php endif; ?>
+            <?php comments_template(); ?>
         </div>
     </div>
 </div>
